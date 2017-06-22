559523aa1ddcf053f0a51cb7ac40a6d42fee8fdb
Dinkar Ganti <dinkar.ganti@gmail.com>
2017-06-21 22:17:12 -0400

    Some refactoring

--- src/Data/Zya/Core/Types.hs
+++ src/Data/Zya/Core/Types.hs
@@ -1,8 +1,31 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Data.Zya.Core.Types where
 import Data.Text
+import Data.Time(UTCTime, getCurrentTime)
 
-type Topic = Text 
+import Control.Applicative((<$>))
+import Control.Monad
+import Control.Monad.Catch
+import Control.Monad.Trans
+import Data.Data
+import Data.Typeable
 
+
+
+subscribe :: Subscription -> Reader 
+subscribe = undefined
+
+publish :: Subscription -> Writer
+publish = undefined
+
+
+writeMessage :: Writer -> Message -> IO Message
+writeMessage w m = do
+  ct <- liftIO $ (getCurrentTime :: IO UTCTime)
+  throwM $ Error (pack "001", pack "error")
+
+
+--------------Application types ---
 data Subscription = 
   Subscription {
     topic :: Topic
@@ -11,28 +34,49 @@ data Subscription =
 }
 
 type ReaderLocation = Integer -- for larger files
-
+type WriterLocation = Integer
 data WriteAudit = WriteAudit {
   writeLocation :: Location
-  , lastWrite :: Timestamp
+  , lastWrite :: UTCTime
   , subscription :: Subscription
 
 }
-data ReaderAudit = ReaderAudit {
-  messages :: Message Pages
-  , pageSize :: PageSize
-  , lastAccess :: Timestamp
 
+data ReaderAudit = ReaderAudit {
+  messages :: [Message]
+  , pageSize :: PageSize
+  , lastAccess :: UTCTime
 }
+newtype PersistentStream = PersistentStream {uFP :: FilePath} 
+newtype ReaderStream = ReaderStream {un :: (ReaderLocation, [Message])}
+newtype WriteStream = WriterStream {unW :: WriterLocation}
 data Reader = Reader {
-  readStream :: Stream
+  readStream :: ReaderStream
   , position :: ReaderLocation
-  , audit :: Audit
+  , fileName :: PersistentStream
+  , audit :: ReaderAudit
 }
+
 data Writer = Writer {
-    writeStream :: Stream
+    writeStream :: WriteStream
+    , wFileName :: PersistentStream
     , writePosition :: WriterLocation
     , waudit :: WriteAudit
 
 }
+
+
+
+
+
+---------- Basic types  ----
+type PageSize = Integer
+
+type Topic = Text 
+type Location = Integer
+type ErrorCode = Text
+
+data Message = Message (UTCTime, Text) deriving (Typeable, Show)
+data Error =  Error (ErrorCode, Text) deriving (Typeable, Show) 
+instance Exception Error
 
