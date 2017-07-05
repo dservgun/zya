7bc81eda4b30830061b9ccb7a1061adab5537497
Dinkar Ganti <dinkar.ganti@gmail.com>
2017-07-05 10:45:20 -0400

    Bump up version.

--- Readme.md
+++ Readme.md
@@ -5,3 +5,7 @@ A realtime webserver allowing users to store and forward custom messages.
 ## [Protocol](Protocol.md)
 
 ## [![Build Status](https://travis-ci.org/dservgun/zya.svg?branch=master)](https://travis-ci.org/dservgun/zya)
+
+## Helpful commands
+
+``` cabal install --only-dependencies --enable-tests ```
--- src/Data/Zya/Core/Service.hs
+++ src/Data/Zya/Core/Service.hs
@@ -1,12 +1,12 @@
 module Data.Zya.Core.Service
-	(
-		-- * The main service 
-		service
-		-- * The supported service profiles
-		, ServiceProfile(..)
-		-- **
+    (
+        -- * The main service 
+        service
+        -- * The supported service profiles
+        , ServiceProfile(..)
+        -- **
 
-	)
+    )
 where 
 import Data.Map as Map
 import Control.Concurrent.STM
@@ -18,26 +18,26 @@ newtype Response = Response {unResponse :: Text} deriving Show
 newtype ClientIdentifier = ClientIdentifier {unClid :: Text} deriving (Show)
 newtype Topic = Topic {unTopic :: Text} deriving Show
 data ClientState = ClientState {
-	topic :: Topic
-	, readPos :: Integer
+    topic :: Topic
+    , readPos :: Integer
 }
 
 {- | The server unifies remote and local processes to manage logging messages.
-	* localClientMap - For each client identifier, list of topics and their read positions.
-	* remoteClientMap - For each process id the state of the topics.
-	* localWriterMap - Write position for a topic.
-	* remoteWriterMap - Write position for a topic. 
-	* serviceMap - A map of the services running on the network.
-	** Note: There should exist only one write position per topic.
+    * localClientMap - For each client identifier, list of topics and their read positions.
+    * remoteClientMap - For each process id the state of the topics.
+    * localWriterMap - Write position for a topic.
+    * remoteWriterMap - Write position for a topic. 
+    * serviceMap - A map of the services running on the network.
+    ** Note: There should exist only one write position per topic.
 
  -}
 data Server = Server {
-	localClientMap :: TVar (Map ClientIdentifier [ClientState])
-	, remoteClientMap :: TVar (Map (ProcessId, ClientIdentifier) [ClientState])
-	, localWriterMap :: TVar (Map Topic Integer)
-	, remoteWriterMap :: TVar (Map (ProcessId, Topic) Integer)
-	,  serviceMap :: TVar (Map (ProcessId, ServiceProfile) Integer)
-	, statistics :: TVar (Map ProcessId ([Request], [Response]))
+    localClientMap :: TVar (Map ClientIdentifier [ClientState])
+    , remoteClientMap :: TVar (Map (ProcessId, ClientIdentifier) [ClientState])
+    , localWriterMap :: TVar (Map Topic Integer)
+    , remoteWriterMap :: TVar (Map (ProcessId, Topic) Integer)
+    ,  serviceMap :: TVar (Map (ProcessId, ServiceProfile) Integer)
+    , statistics :: TVar (Map ProcessId ([Request], [Response]))
 }
 
 type ServiceRange = (Int, Int) 
@@ -46,13 +46,13 @@ type ServiceRange = (Int, Int)
 -}
 defaultSimpleConfiguration :: [(ServiceProfile, ServiceRange)]
 defaultSimpleConfiguration = [(WebServer, (3, 10)), (DatabaseServer, (3, 10)), 
-						(Reader, (3, 10)), 
-						(Writer, (3, 10))]
+                        (Reader, (3, 10)), 
+                        (Writer, (3, 10))]
 
 {- | Services that can be started on a cloud. -}
 data ServiceProfile = 
-	WebServer | DatabaseServer | Reader | Writer
-	deriving(Show)
+    WebServer | DatabaseServer | Reader | Writer
+    deriving(Show)
 
 newtype Service = Service {unService :: ServiceProfile -> [NodeId] -> Process[NodeId]}
 
