648264ab0d9d0210c3bd9581815928b279921268
dinkar <dinkar.ganti@gmail.com>
2017-12-29 20:21:06 -0500

    minor fixes

--- hsclient/Data/Zya/Client/SimpleClient.hs
+++ hsclient/Data/Zya/Client/SimpleClient.hs
@@ -2,13 +2,13 @@
 
 module Data.Zya.Client.SimpleClient where 
 
-import 				Control.Concurrent(forkIO)
-import				Control.Monad (forever, unless)
-import				Control.Monad.Trans(liftIO)
-import				Network.Socket (withSocketsDo)
-import qualified	Data.Text as T 
-import qualified 	Data.Text.IO as T 
-import qualified 	Network.WebSockets as WS
+import        Control.Concurrent(forkIO)
+import        Control.Monad (forever, unless)
+import        Control.Monad.Trans(liftIO)
+import        Network.Socket (withSocketsDo)
+import qualified  Data.Text as T 
+import qualified  Data.Text.IO as T 
+import qualified  Network.WebSockets as WS
 import Data.Monoid
 import Data.Aeson
 import Data.Text.Encoding(encodeUtf8)
@@ -17,20 +17,19 @@ parseJson :: T.Text -> Either String Value
 parseJson = eitherDecodeStrict . encodeUtf8 
 app :: WS.ClientApp () 
 app conn = do 
-	putStrLn "Connected!"
-	_ <- forkIO $ forever $ do 
+  putStrLn "Connected!"
+  _ <- forkIO $ forever $ do 
           msg <- WS.receiveData conn 
           liftIO $ putStrLn $ show msg
-          liftIO $ putStrLn $ show $ parseJson msg 
-
-	let loop = do 
-		line <- T.getLine
-		unless (T.null line) $ putStrLn $ show $ ("receved " :: T.Text) <> line
-	loop
-	WS.sendClose conn("Bye!" :: T.Text)
+          liftIO $ putStrLn $ show $ parseJson msg
+  let loop = do 
+              line <- T.getLine
+              unless (T.null line) $ putStrLn $ show $ ("receved " :: T.Text) <> line
+  loop
+  WS.sendClose conn("Bye!" :: T.Text)
 
 
 mainApp :: IO () 
 mainApp = do 
-	[portNumber] <- map read <$> getArgs
-	withSocketsDo $ WS.runClient "localhost" portNumber "/" app
+  [portNumber] <- map read <$> getArgs
+  withSocketsDo $ WS.runClient "localhost" portNumber "/" app
